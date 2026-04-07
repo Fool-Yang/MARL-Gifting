3b2ae49bd32c01c3ac5d6222631466e0a0760412
Yang <leo19970804@gmail.com>
2026-04-08 00:44:17 +0100

    confidence intervals

--- run_games.py
+++ run_games.py
@@ -2,14 +2,25 @@ import games
 from MCFS import *
 from time import time
 
-experiments = 1
-iterations = 2000
+experiments = 50
+# iterations must be >= the number of actions on turn one or the stats won't work
+iterations = 1000
 
 # list of game classes to create game objects
 Games = (
-    games.FreeMoney,
+    #games.BiasedRPS,
+    #games.FreeMoney,
+    #games.PeerRewardingWrapper(games.FreeMoney),
+    games.BeNice,
+    games.PeerRewardingWrapper(games.BeNice),
     games.Prisoners,
-    games.StagHunt
+    games.PeerRewardingWrapper(games.Prisoners),
+    games.StagHunt,
+    games.PeerRewardingWrapper(games.StagHunt),
+    games.StagHunt3A,
+    games.PeerRewardingWrapper(games.StagHunt3A),
+    games.StagHunt3P,
+    games.PeerRewardingWrapper(games.StagHunt3P),
 )
 
 for game in Games:
@@ -25,13 +36,14 @@ for game in Games:
             actions = mcf.search(iterations)
             game_running, rewards = g.step(actions)
         f = open(record_file, 'a')
-        f.write(str(mcf.W) + '\n')
-        f.write(str(mcf.N) + '\n\n')
+        f.write(str(mcf.Q) + '\n')
+        f.write(str(mcf.P) + '\n\n')
         f.close()
     print("Game", g.name, "ended. Total time:", time() - t0)
 
 # plot
-import numpy as np
+import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 
 for game in Games:
@@ -39,42 +51,32 @@ for game in Games:
     f = open(record_file, 'r')
     data = f.read().strip().split('\n\n')
     f.close()
-    format_example = data[0].split('\n')
-    total_estimated_rewards = eval(format_example[0])
-    total_empirical_distributions = eval(format_example[1])
-    # clear data
-    for agent_id in total_estimated_rewards:
-        for action in total_estimated_rewards[agent_id]:
-            total_estimated_rewards[agent_id][action] = np.zeros(len(total_estimated_rewards[agent_id][action]))
-            total_empirical_distributions[agent_id][action] = np.zeros(len(total_empirical_distributions[agent_id][action]))
-    for game_run in data:
-        W, N = map(eval, game_run.split('\n'))
-        for agent_id in W:
-            for action in W[agent_id]:
-                total_rewards = np.array(W[agent_id][action])
-                count = np.array(N[agent_id][action])
-                total_estimated_rewards[agent_id][action] += total_rewards/count
-                total_empirical_distributions[agent_id][action] += count/np.array(range(1, len(count) + 1))
-    # take average
-    for agent_id in total_estimated_rewards:
-        for action in total_estimated_rewards[agent_id]:
-            total_estimated_rewards[agent_id][action] /= len(data)
-            total_empirical_distributions[agent_id][action] /= len(data)
-    # plot
-    for agent_id in total_estimated_rewards:
-        # reward
+    for agent_id in eval(data[0].split('\n')[0]):
+        rows_Q = []
+        rows_P = []
+        for trail, game_run in enumerate(data):
+            Q, P = map(eval, game_run.split('\n'))
+            for action in Q[agent_id]:
+                action_name = game.action_names[agent_id][action]
+                for i in range(len(Q[agent_id][action])):
+                    rows_Q.append({
+                        "action": action_name,
+                        "trial": trail,
+                        "iteration": i,
+                        "average reward": Q[agent_id][action][i]
+                        })
+                    rows_P.append({
+                        "action": action_name,
+                        "trial": trail,
+                        "iteration": i,
+                        "policy": P[agent_id][action][i]
+                        })
+        df_Q = pd.DataFrame(rows_Q)
+        df_P = pd.DataFrame(rows_P)
         fig_reward, ax_reward = plt.subplots()
-        ax_reward.set_xlabel("iteration")
-        ax_reward.set_ylabel("average reward")
+        sns.lineplot(data=df_Q, x="iteration", y="average reward", hue="action", errorbar="ci", n_boot=200)
         ax_reward.set_title(game.name + ": agent " + str(agent_id))
-        # strategy
         fig_strategy, ax_strategy = plt.subplots()
-        ax_strategy.set_xlabel("iteration")
-        ax_strategy.set_ylabel("empirical distribution")
+        sns.lineplot(data=df_P, x="iteration", y="policy", hue="action", errorbar="ci", n_boot=200)
         ax_strategy.set_title(game.name + ": agent " + str(agent_id))
-        for action in total_estimated_rewards[agent_id]:
-            ax_reward.plot(total_estimated_rewards[agent_id][action], label = game.action_names[agent_id][action])
-            ax_reward.legend(loc='best')
-            ax_strategy.plot(total_empirical_distributions[agent_id][action], label = game.action_names[agent_id][action])
-            ax_strategy.legend(loc='best')
 plt.show()
