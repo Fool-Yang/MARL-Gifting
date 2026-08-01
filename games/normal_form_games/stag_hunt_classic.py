fe880c8a73d43231fc640da73e648e189e829f2b
Yang <leo19970804@gmail.com>
2026-08-01 16:47:59 +0100

    Update stag_hunt_classic.py

--- games/normal_form_games/stag_hunt_classic.py
+++ games/normal_form_games/stag_hunt_classic.py
@@ -7,7 +7,7 @@ class StagHuntClassic(NormalFormGame):
 
     def __init__(self, max_t=1):
         self.reward_matrix = (
-            ((1, 1), (0, 0.5)),
-            ((0.5, 0), (0.4, 0.4))
+            ((1, 1), (0.1, 0.8)),
+            ((0.8, 0.1), (0.5, 0.5))
         )
         super().__init__(max_t)
